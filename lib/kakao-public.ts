5760b3e1656011c21b3a08bd1190c264ea6d39a3
JAEHAN <jaehanlee@JAEHANui-iMacPro.local>
2026-03-29 09:57:31 +0900

    Fix: 카카오톡 채널 링크 수정

--- lib/kakao-public.ts
+++ lib/kakao-public.ts
@@ -1,3 +1,3 @@
 /** Public Kakao Business Channel (matches contact page & channel button). */
 export const KAKAO_CHANNEL_PUBLIC_URL =
-  "https://pf.kakao.com/_thinkad" as const;
+  "http://pf.kakao.com/_FPxaju/chat" as const;
